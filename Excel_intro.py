ba57d322a146fc9e30bd71318335c4d55901e005
DadiPallavi <dadipallavi5@gmail.com>
2026-05-05 16:59:51 +0530

    Create Excel_intro.py

---
+++ Excel_intro.py
@@ -0,0 +1,25 @@
+1. What is Excel?
+ A) excel is a spreadsheet tool of Microsoft which is used to store the data in large amount and doing calucalation, and analyze the data 
+2. What is a workbook? 
+A) a workbook is a excel file which contains one or more than one worksheets 
+3. What is a spreadsheet? 
+A) spreadsheet is a grid of rows and cloumns which is used to store and manage the data. Each sheet is called worksheet or spreadsheet 
+4. When was Excel developed? 
+A) 1985 
+5. Why is Excel used?
+ A) to store large amount of data Data analaysis Calculation using formulas Creating charts and reports Manages the large set of data 
+6. What is the difference between Excel and MySQL? 
+A) excel is a spreadsheet The data size in excel is small to medium It is used to analysis,reports We can perform operation using formulas
+ MYSQL:-- It ia database It stores very large amount of data The usage is for data storeage and management The languge is sql 
+7. What are the uses of Excel? 
+A) Budgeting Data entry Data visualization (charts) Financial analysis Reporting Inventory tracking 
+8. How often are Excel versions updated?
+ A) major version every 2-3 years Frequent updates is monthly /regular updates
+ 9. When did Excel start? 
+A)  1985 
+10. Who introduced Excel? 
+A)microsoft 
+11. What was the first version of Excel? 
+Excel1.0 
+12. How can we check the Excel version? 
+First version: Excel 1.0 Released for Mac (1985) Windows version came later in 1987
